ea12a3559e1f86ba847fa6e7e067431b6ab01d57
luca.malisan <luca.malisan@informaticon.com>
2024-06-29 14:37:04 +0200

    commit safe

--- main/java/src/LoginApplication.java
+++ main/java/src/LoginApplication.java
@@ -2,14 +2,23 @@ package src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import src.config.RsaKeyProperties;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
-public class LoginApplication {
+@Configuration
+public class LoginApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LoginApplication.class);
     }
 }
